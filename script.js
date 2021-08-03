3d54e40843980f6fe5765d577917bf7b4e49f681
kiabel <85337763+kiabel@users.noreply.github.com>
2021-08-03 16:29:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ function compute(){
   let interest = principal.value * years.value * rate.value / 100;
   futYear = new Date().getFullYear() + parseInt(years.value);
   result = document.getElementById("result")
-  result.innerHTML = "<p>If you deposit <mark>"+ principal.value +"</mark>,<br>an interest rate of <mark>" + rate.value + "%</mark>.<br>You will receive an amount of <mark>"+interest+"</mark>,<br>in the year <mark>"+futYear+"</mark></p>";
+  result.innerHTML = "<br> <h>Result: <br>"+"<p>If you deposit <mark>"+ principal.value +"</mark>,<br>an interest rate of <mark>" + rate.value + "%</mark>.<br>You will receive an amount of <mark>"+interest+"</mark>,<br>in the year <mark>"+futYear+"</mark></p>";
 
 }
 
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval + "%";
 }
 
